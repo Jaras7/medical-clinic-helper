10c49305a7123a0a9b8020fcfed9337e3ed90e60
Jaroslaw Bryk <jaroslawbryk@MacBook-Pro-Jarosaw.local>
2018-01-12 14:10:29 +0100

    initial commit

---
+++ src/main/java/com/medical/clinic/helper/boot/MedicalClinicHelperApplication.java
@@ -0,0 +1,12 @@
+package com.medical.clinic.helper.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicHelperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MedicalClinicHelperApplication.class, args);
+  }
+}
---
+++ src/test/java/com/medical/clinic/helper/boot/MedicalClinicHelperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.medical.clinic.helper.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedicalClinicHelperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
